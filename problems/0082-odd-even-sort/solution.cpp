2c2e28291fc19940a1e91f4e0809f26f7a0e3135
bvanhoa72-beep <bvanhoa72-beep@users.noreply.github.com>
2026-07-26 23:41:04 -0600

    Add problem 82 odd-even sort

---
+++ problems/0082-odd-even-sort/solution.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> odds;
+    vector<int> evens;
+
+    for (int i = 0; i < n; ++i)
+    {
+        int x;
+        cin >> x;
+
+        if (x % 2 != 0)
+        {
+            odds.push_back(x);
+        }
+        else
+        {
+            evens.push_back(x);
+        }
+    }
+
+    sort(odds.begin(), odds.end());
+    sort(evens.begin(), evens.end());
+
+    bool first = true;
+    for (int x : odds)
+    {
+        if (!first)
+        {
+            cout << ' ';
+        }
+        cout << x;
+        first = false;
+    }
+
+    for (int x : evens)
+    {
+        if (!first)
+        {
+            cout << ' ';
+        }
+        cout << x;
+        first = false;
+    }
+
+    cout << '\n';
+    return 0;
+}
